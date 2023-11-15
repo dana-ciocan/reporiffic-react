e6cdcd2081a721ce6a50c05892ce9168d948b668
Dana Ciocan <danaciocan@economist.com>
2023-11-15 11:49:27 +0000

    Show list of repos on front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css'
+import { Repo } from './components/repo/Repo';
+import { UserWelcome } from './components/user-welcome/UserWelcome';
+import { useOctokitTeamData } from './hooks/useOctokitTeamData';
+import { useOctokitTeamRepos } from './hooks/useOctokitTeamRepos';
+import { useOctokitUserData } from './hooks/useOctokitUserData';
+
 
 export const App = () => {
+  const user = useOctokitUserData();
+  const team = useOctokitTeamData();
+  const repos = useOctokitTeamRepos();
+
+  const name = (user.name || user.login || 'there').split(' ')?.[0];
+
   return (
     <>
-      <h1>Welcome to reporiffic!</h1>
+      <UserWelcome name={name} team={team.name} />
+      <p>These are the repos {team.name} manages:</p>
+      {repos.map(repo => <Repo name={repo.name} />)}
     </>
   )
 }
