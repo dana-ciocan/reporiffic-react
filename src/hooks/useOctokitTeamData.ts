7383451adcabda91d6442dfd84c425542f3dc248
Dana Ciocan <danaciocan@economist.com>
2023-11-15 11:34:36 +0000

    Create hook to get a team's data

---
+++ src/hooks/useOctokitTeamData.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { getOctokit } from '../utils/get-octokit';
+import { Team } from '../types/team';
+
+export const useOctokitTeamData = (): Team => {
+  const [team, setTeam] = useState({ name: ''});
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await getOctokit()
+        .request('GET /orgs/{org}/teams/{team_slug}', {
+            org: import.meta.env.VITE_GITHUB_OWNER,
+            team_slug: import.meta.env.VITE_GITHUB_TEAM,
+          })
+        .then(({ data }) => {
+          setTeam({  name: data?.name || '' });
+        })
+        .catch((err) => console.log(err));
+    };
+    onLoad();
+  }, []);
+  return team;
+};
