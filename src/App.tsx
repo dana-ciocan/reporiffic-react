c99fb5842bdfd8727d5921e0d4a50c467f430487
Dana Ciocan <danaciocan@economist.com>
2023-11-29 09:13:11 +0000

    Remove repo logic from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
-import { Repo } from './components/repo/Repo';
 import { WelcomeBanner } from './components/welcome-banner/WelcomeBanner';
 import { useOctokitTeamData } from './hooks/useOctokitTeamData';
-import { useOctokitTeamRepos } from './hooks/useOctokitTeamRepos';
 import { useOctokitUserData } from './hooks/useOctokitUserData';
 
 
 export const App = () => {
   const user = useOctokitUserData();
   const team = useOctokitTeamData();
-  const repos = useOctokitTeamRepos();
 
   const name = (user.name || user.login || 'there').split(' ')?.[0];
   const teamName = (team.name || 'your team');
@@ -17,16 +14,6 @@ export const App = () => {
   return (
     <>
       <WelcomeBanner name={name} team={teamName} />
-      <table>
-        <thead>
-        <tr>
-          <th>Repo name</th><th>Number of open PRs</th>
-        </tr>
-        </thead>
-        <tbody>
-          {repos.map(repo => <Repo key={repo.name} name={repo.name} />)}
-        </tbody>
-      </table>
     </>
   )
 }
